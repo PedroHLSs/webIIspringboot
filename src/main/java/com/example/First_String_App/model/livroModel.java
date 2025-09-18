741ebe384d90f327c7a6f3655a7fa3abee0d81bc
PedroHLSs <pedro.lopes@estudante.iftm.edu.br>
2025-09-17 21:23:58 -0300

    Criação do livroModel

--- src/main/java/com/example/First_String_App/model/livroModel.java
+++ src/main/java/com/example/First_String_App/model/livroModel.java
@@ -1,5 +1,66 @@
 package com.example.First_String_App.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "livros")
+
 public class livroModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String titulo;
+    @Column
+    private String autor;
+    @Column
+    private LocalDateTime ano;
+    @Column
+    private String editora;
+    @Column
+    private Boolean disponivel;
     
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public LocalDateTime getAno() {
+        return ano;
+    }
+    public void setAno(LocalDateTime ano) {
+        this.ano = ano;
+    }
+    public String getEditora() {
+        return editora;
+    }
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
